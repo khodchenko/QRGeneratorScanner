e610f5af9f9987bb644cfc069b69a479d2810ee7
khodchenko <khoch3nk0@gmail.com>
2021-12-03 17:30:24 +0100

    add permission checker

--- app/src/main/java/com/example/qrgeneratorscanner/MainActivity.kt
+++ app/src/main/java/com/example/qrgeneratorscanner/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.example.qrgeneratorscanner
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.security.Permission
 
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var im: ImageView
-    lateinit var bGenerate : Button
-    lateinit var bScanner : Button
-
+    lateinit var bGenerate: Button
+    lateinit var bScanner: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +25,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         bGenerate = findViewById(R.id.bGenerate)
         bScanner = findViewById(R.id.bScan)
-        bScanner.setOnClickListener{
+        bScanner.setOnClickListener {
+            checkCameraPermission()
+        }
+    }
+
+    private fun checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 12)
+        } else {
             startActivity(Intent(this, ScannerActivity::class.java))
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == 12 ){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                startActivity(Intent(this, ScannerActivity::class.java))
+            }
         }
     }
 }
